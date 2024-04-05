c49bedbe6330ba3bb90d98918a61b24898f28327
mkaufman385 <mkaufman385@gmail.com>
2024-04-05 18:32:24 -0400

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
-const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
+const { checkUsernameExists, validateRoleName } = require("./auth-middleware");
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const bcrypt = require('bcryptjs')
+const User = require('../users/users-model')
+
 
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
@@ -14,8 +17,9 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "role_name": "angel"
     }
    */
-});
-
+  const { username, password } = req.body;
+  const {role_name} = req;
+  const hash = bcrypt.hashSync(password, 8)
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
   /**
